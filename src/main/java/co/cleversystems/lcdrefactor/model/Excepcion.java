b5e041acd92a22315b7af2169321496730700742
James <jaimea111@gmail.com>
2018-07-30 13:05:30 -0500

    Initial push

---
+++ src/main/java/co/cleversystems/lcdrefactor/model/Excepcion.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.cleversystems.lcdrefactor.model;
+
+/**
+ *
+ * @author james
+ */
+public class Excepcion extends Exception{
+    public Excepcion() {
+
+    }
+    public Excepcion( String message ) {
+        super( message );
+    }
+
+    public Excepcion( Throwable cause ) {
+        super( cause );
+    }
+
+    public Excepcion( String message, Throwable cause ) {
+        super( message, cause );
+    }
+
+    public Excepcion( String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace ) {
+        super( message, cause, enableSuppression, writableStackTrace );
+    }
+}
---
+++ src/main/java/co/cleversystems/lcdrefactor/model/Test.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.cleversystems.lcdrefactor.model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Jaime Alonso - Clever Systems ©
+ */
+public class Test {
+    public static void main(String args[]){
+        ArrayList lista = new ArrayList();
+        lista.add("Hola");
+        System.out.println(lista.get(0));
+    }
+}
